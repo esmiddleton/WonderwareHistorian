279f13d1c395c6df813c8fdbefc384650e45a5c0
esmiddleton <elliott@middletonclan.net>
2016-12-02 08:41:45 -0800

    Update StateSummariesWhenNoChange.sql

--- Queries/StateSummariesWhenNoChange.sql
+++ Queries/StateSummariesWhenNoChange.sql
@@ -6,6 +6,20 @@ states that actually occurred during the period queried. In many reporting appli
 to still include a row for a state that didn't occur (e.g. report the running hours for a pump was "0"
 when it was off for the entire period).
 
+This query returns:
+
+	SysTimeHour   0     0
+	SysPulse      15    900000
+
+In this case, the values for SysTimeHour would be NULL without the ISNULL function. Without the extra OUTER JOIN, 
+it would return:
+
+	SysPulse      15    900000
+
+The INNER REMOTE JOIN hint is required to force SQL Server to send the tag list to the Historian's OLE DB 
+provider--without it, newer releases of SQL Server will "optimize" the query by trying to apply the criteria 
+itself, rather than giving Historian the tag list (which Historian requires).
+
 */
 
 SET NOCOUNT ON
